8cc62b4eb0e8e9e2d8c4019c66db8480abf2e660
DICAVI86 <delcastillovial@gmail.com>
2024-06-07 19:18:05 -0400

    feat/Se creo el componente de Listado y Buscador

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
+import React from 'react';
+import { useState } from 'react';
 import './App.css'
+import { BaseColaboradores } from './assets/BaseColaboradores';
+import Listado from './components/Listado';
+import Buscador from './components/Buscador';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
 
 function App() {
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores);
+  const [buscar, setBuscar] = useState("");
+
+  const agregarColaborador = (nombre, correo, edad, cargo, telefono) => {
+    const nuevoColaborador = {
+      id: (colaboradores.length + 1).toString(),
+      nombre, correo, edad, cargo, telefono
+    };
+    console.log(nombre, correo, edad, cargo, telefono);
+    setColaboradores([...colaboradores, nuevoColaborador]);
+  };
+
+  const colaboradoresFiltrados = colaboradores.filter((colaborador) => 
+    colaborador.nombre.toLowerCase().includes(buscar.toLowerCase()) ||
+    colaborador.correo.toLowerCase().includes(buscar.toLowerCase()) ||
+    colaborador.edad.toString().includes(buscar.toLowerCase()) ||
+    colaborador.cargo.toLowerCase().includes(buscar.toLowerCase()) ||
+    colaborador.telefono.toLowerCase().includes(buscar.toLowerCase())
+  );
 
 
   return (
     <>
-      
+     <Container>
+        <Row>
+          <Col>
+            <Buscador buscar={buscar} setBuscar={setBuscar} />
+          </Col>
+        </Row>
+        
+        <Row>
+          <Col md={6} sm={12}>
+            <Listado colaboradores={colaboradoresFiltrados} />
+          </Col>
+          {/* <Col md={6}>
+            <Formulario agregarColaborador={agregarColaborador} />
+          </Col> */}
+        </Row>
+      </Container>
     </>
   )
 }
